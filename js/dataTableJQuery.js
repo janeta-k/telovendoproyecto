1fd7f9ed8bce8b9b521e54246be6b915aa6f087f
Janet <janetsanti892@gmail.com>
2022-03-14 18:29:56 -0300

    Favor revisar y modificar

---
+++ js/dataTableJQuery.js
@@ -0,0 +1,17 @@
+$(document).ready( function () {
+    $('#myTable').DataTable();
+} );
+
+/*
+$('#myTable').DataTable( {
+    responsive: true
+} );
+*/
+
+/*
+$(document).ready( function () {
+    $('#myTable').DataTable({
+        responsive: true
+    });
+} );
+*/
